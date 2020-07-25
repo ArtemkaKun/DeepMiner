3079a56fe110d3e5458f693d0b675afb70525af2
Artem Yurchenko <artem@studiovector.pl>
2020-07-25 17:14:05 +0200

    Coal spawn algo was added

--- Assets/Scripts/Systems/GroundSpawnSystem.cs
+++ Assets/Scripts/Systems/GroundSpawnSystem.cs
@@ -13,6 +13,7 @@ public class GroundSpawnSystem : MonoBehaviour
     private const int StartLayerForEmptyCells = -2;
     private const int StartLayerForStonesCells = -4;
     private const int StartLayerForLavaCells = -19;
+    private const int StartLayerForCoalCells = -2;
     
     private const int LayersCountForStoneCellSpawnIncrease = 10;
     private const int LayersCountForLavaCellSpawnIncrease = 20;
@@ -20,7 +21,8 @@ public class GroundSpawnSystem : MonoBehaviour
     private const float MakeEmptyCellChance = 0.1f;
     private const float MaxStoneCellSpawnChance = 0.8f;
     private const float MaxLavaCellSpawnChance = 0.4f;
-    
+
+    private const float SpawnCoalCellChance = 0.2f;
     private float SpawnStoneCellChance = 0.5f;
     private float SpawnLavaCellChance = 0.1f;
     
@@ -56,6 +58,8 @@ public class GroundSpawnSystem : MonoBehaviour
             {
                 SpawnStoneCell(i, ref groundCells);
                 SpawnLavaCell(i, ref groundCells);
+                
+                SpawnOre(i, ref groundCells);
                 
                 EntitiesManager.EntityManager.SetComponentData(groundCells[i], new Translation
                 {
@@ -112,6 +116,27 @@ public class GroundSpawnSystem : MonoBehaviour
         {
             EntitiesManager.EntityManager.DestroyEntity(groundCells[i]);
             groundCells[i] = EntitiesManager.EntityManager.Instantiate(EntitiesManager.GetEntity("LavaCell"));
+        }
+    }
+
+    private void SpawnOre(int i, ref NativeArray<Entity> groundCells)
+    {
+        var randomFloat = new Unity.Mathematics.Random((uint) _randomSeed.Next()).NextFloat();
+
+        switch (randomFloat)
+        {
+            case var _ when randomFloat <= SpawnCoalCellChance:
+                SpawnCoal(i, ref groundCells);
+                break;
+        }
+    }
+
+    private void SpawnCoal(int i, ref NativeArray<Entity> groundCells)
+    {
+        if (_ySpawnOffset < StartLayerForCoalCells)
+        {
+            EntitiesManager.EntityManager.DestroyEntity(groundCells[i]);
+            groundCells[i] = EntitiesManager.EntityManager.Instantiate(EntitiesManager.GetEntity("CoalCell"));
         }
     }
 }
