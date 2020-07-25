72baa33ab3a3c0a2fbbb92a542c1c513d99e6b4f
Artem Yurchenko <artem@studiovector.pl>
2020-07-25 11:37:37 +0200

    GroundSpawnSystem was added

---
+++ Assets/Scripts/Entities/EntitiesManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class EntitiesManager : MonoBehaviour
+{
+    private static EntityManager _entityManager;
+
+    public static EntityManager EntityManager => _entityManager;
+
+    private static Dictionary<string, Entity> _gameEntities;
+    private BlobAssetStore _blobAssetStore;
+
+    private void Awake()
+    {
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        _blobAssetStore = new BlobAssetStore();
+        _gameEntities = new Dictionary<string, Entity>();
+        
+        CreateGroundCellEntity();
+    }
+
+    private void CreateGroundCellEntity()
+    {
+        var groundCellPrefab = Resources.Load<GameObject>("Prefabs/GroundCell");
+        var groundCellEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(groundCellPrefab,
+            GameObjectConversionSettings.FromWorld(_entityManager.World, _blobAssetStore));
+        
+        _gameEntities["GroundCell"] = groundCellEntity;
+    }
+
+    public static Entity GetEntity(string entityName)
+    {
+        return _gameEntities[entityName];
+    }
+
+    private void OnDestroy()
+    {
+        _blobAssetStore.Dispose();
+    }
+}
---
+++ Assets/Scripts/Systems/GroundSpawnSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class GroundSpawnSystem : MonoBehaviour
+{
+    private const int MaxGroundCells = 540;
+    private const int MaxGroundCellInRow = 18;
+    
+    private void Start()
+    {
+        var groundCells = new NativeArray<Entity>(MaxGroundCells, Allocator.Temp);
+        EntitiesManager.EntityManager.Instantiate(EntitiesManager.GetEntity("GroundCell"), groundCells);
+
+        var xSpawnOffset = 0;
+        var ySpawnOffset = 0;
+        for (var i = 0; i < groundCells.Length; i++)
+        {
+            if (i != 0 && i % MaxGroundCellInRow == 0)
+            {
+                --ySpawnOffset;
+                xSpawnOffset = 0;
+            }
+            
+            EntitiesManager.EntityManager.SetComponentData(groundCells[i], new Translation
+            {
+                Value = new float3(xSpawnOffset, ySpawnOffset, 0)
+            });
+            
+            ++xSpawnOffset;
+        }
+
+        groundCells.Dispose();
+    }
+}
