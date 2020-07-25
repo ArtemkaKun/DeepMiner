041ede2bf63ebeafd3f31874c747a822a18b6afa
Artem Yurchenko <artem@studiovector.pl>
2020-07-25 15:41:23 +0200

    Empty cells generation algo was implemented

--- Assets/Scripts/Systems/GroundSpawnSystem.cs
+++ Assets/Scripts/Systems/GroundSpawnSystem.cs
@@ -3,35 +3,63 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using Random = System.Random;
 
 public class GroundSpawnSystem : MonoBehaviour
 {
     private const int MaxGroundCells = 540;
     private const int MaxGroundCellInRow = 18;
+    private const int StartLayerForEmptyCells = -2;
+    private const float MakeEmptyCellChance = 0.1f;
     
+    private Random _randomSeed;
+    private int _xSpawnOffset;
+    private int _ySpawnOffset;
+
     private void Start()
     {
+        _randomSeed = new Random();
+
         var groundCells = new NativeArray<Entity>(MaxGroundCells, Allocator.Temp);
         EntitiesManager.EntityManager.Instantiate(EntitiesManager.GetEntity("GroundCell"), groundCells);
 
-        var xSpawnOffset = 0;
-        var ySpawnOffset = 0;
+        SpawnGround(groundCells);
+        groundCells.Dispose();
+    }
+
+    private void SpawnGround(NativeArray<Entity> groundCells)
+    {
         for (var i = 0; i < groundCells.Length; i++)
         {
-            if (i != 0 && i % MaxGroundCellInRow == 0)
-            {
-                --ySpawnOffset;
-                xSpawnOffset = 0;
-            }
-            
-            EntitiesManager.EntityManager.SetComponentData(groundCells[i], new Translation
-            {
-                Value = new float3(xSpawnOffset, ySpawnOffset, 0)
-            });
-            
-            ++xSpawnOffset;
-        }
+            CheckForNewRow(i);
 
-        groundCells.Dispose();
+            if (SetEmptyCell())
+            {
+                EntitiesManager.EntityManager.DestroyEntity(groundCells[i]);
+            }
+            else
+            {
+                EntitiesManager.EntityManager.SetComponentData(groundCells[i], new Translation
+                {
+                    Value = new float3(_xSpawnOffset, _ySpawnOffset, 0)
+                });
+            }
+
+            ++_xSpawnOffset;
+        }
+    }
+
+    private void CheckForNewRow(int i)
+    {
+        if (i == 0 || i % MaxGroundCellInRow != 0) return;
+
+        --_ySpawnOffset;
+        _xSpawnOffset = 0;
+    }
+
+    private bool SetEmptyCell()
+    {
+        var randomFloat = new Unity.Mathematics.Random((uint) _randomSeed.Next()).NextFloat();
+        return _ySpawnOffset < StartLayerForEmptyCells && randomFloat < MakeEmptyCellChance;
     }
 }
