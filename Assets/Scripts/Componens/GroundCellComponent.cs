7aa3a2bc201c1d0782c8171cb8bb80ee04a65bc7
Artem Yurchenko <artem@studiovector.pl>
2020-07-27 20:31:42 +0200

    GroundCellComponent was added

---
+++ Assets/Scripts/Componens/GroundCellComponent.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct GroundCellComponent : IComponentData
+{
+    public float Durability;
+    public float Weight;
+    public float Cost;
+}
