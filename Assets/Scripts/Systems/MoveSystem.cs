cdcaa55b7829fec9344ee8995379acac04a00086
Artem Yurchenko <artem@studiovector.pl>
2020-07-27 22:26:30 +0200

    Move system was added

---
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class MoveSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref MoveComponent moveComponent) =>
+        {
+            var horizontalAxisValue = Input.GetAxis("Horizontal");
+            
+            if (Math.Abs(horizontalAxisValue) > 0.001f)
+            {
+                translation.Value.x += horizontalAxisValue * moveComponent.Speed * Time.DeltaTime;
+            }
+        });
+    }
+}
