2cfdf92a73f7de535d2893d53c2c4e824ce6692b
Artem Yurchenko <artem@studiovector.pl>
2020-08-08 11:32:57 +0200

    New drill sprite + movement system was adjusted

--- Assets/Scripts/Systems/MoveSystem.cs
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -4,6 +4,7 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
+using Math = System.Math;
 
 [UpdateAfter(typeof(DrillSystem))]
 public class MoveSystem : ComponentSystem
@@ -19,12 +20,12 @@ public class MoveSystem : ComponentSystem
 
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref MoveComponent moveComponent, ref PhysicsVelocity velocity, ref PhysicsMass mass, ref DrillComponent drill, ref Translation translation) =>
+        Entities.ForEach((ref MoveComponent moveComponent, ref PhysicsVelocity velocity, ref PhysicsMass mass, ref DrillComponent drill, ref Translation translation, ref Rotation rotation) =>
         {
             if (drill.IsDrilling) return;
             
             RestrictPlayerRotation(ref mass);
-            MovePlayer(ref velocity, ref moveComponent);
+            MovePlayer(ref velocity, ref moveComponent, ref rotation);
             
             if (translation.Value.y <= _groundSpawner.YSpawnOffset + SpawnNewLayerOffset)
             {
@@ -40,13 +41,18 @@ public class MoveSystem : ComponentSystem
         mass.InverseInertia = new float3(0);
     }
 
-    private void MovePlayer(ref PhysicsVelocity velocity, ref MoveComponent moveComponent)
+    private void MovePlayer(ref PhysicsVelocity velocity, ref MoveComponent moveComponent, ref Rotation rotation)
     {
         var horizontalAxisValue = Input.GetAxis("Horizontal");
         var verticalAxisValue = Input.GetAxis("Vertical");
-        
+
+        if (Math.Abs(horizontalAxisValue) > 0.01f)
+        {
+            rotation.Value = Quaternion.Euler(0, horizontalAxisValue < 0 ? 180 : 0, 0);
+        }
+
         velocity.Linear += new float3(horizontalAxisValue * moveComponent.Speed,
-            verticalAxisValue * moveComponent.HorizontalForce, 0) * Time.DeltaTime;
+                               verticalAxisValue * moveComponent.HorizontalForce, 0) * Time.DeltaTime;
     }
 
     private void MoveCamera(ref Translation translation)
