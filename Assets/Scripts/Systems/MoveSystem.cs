0f70b755d5d5e580536bd1d3da43f9868cabe78d
Artem Yurchenko <artem@studiovector.pl>
2020-08-09 14:34:10 +0200

    DynamicGroundSystemSpawner was added

--- Assets/Scripts/Systems/MoveSystem.cs
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -12,9 +12,6 @@ using Math = System.Math;
 
 public class MoveSystem : ComponentSystem
 {
-    private const int SpawnNewLayerOffset = 6;
-    
-    private GroundSpawnSystem _groundSpawner;
     private Material _currentMaterial;
     private Material _sidePlayerMaterial;
     private Material _frontPlayerMaterial;
@@ -24,7 +21,6 @@ public class MoveSystem : ComponentSystem
     {
         base.OnStartRunning();
         
-        _groundSpawner = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GroundSpawnSystem>();
         _sidePlayerMaterial = Resources.Load<Material>("Materials/PlayerMaterial");
         _frontPlayerMaterial = Resources.Load<Material>("Materials/PlayerMaterialFront");
         
@@ -36,37 +32,27 @@ public class MoveSystem : ComponentSystem
 
     protected override void OnUpdate()
     {
-        Entities.ForEach((Entity entity, ref MoveComponent moveComponent, ref PhysicsVelocity velocity, ref DrillComponent drill, ref Translation translation, ref Rotation rotation) =>
+        Entities.ForEach((ref MoveComponent moveComponent, ref PhysicsVelocity velocity, ref DrillComponent drill, ref Translation translation, ref Rotation rotation) =>
         {
             if (drill.IsDrilling) return;
             
-            MovePlayer(ref entity, ref velocity, ref moveComponent, ref rotation, ref translation);
-            
-            if (translation.Value.y <= _groundSpawner.YSpawnOffset + SpawnNewLayerOffset)
+            var horizontalAxisValue = Input.GetAxis("Horizontal");
+            var verticalAxisValue = Input.GetAxis("Vertical");
+        
+            if (Math.Abs(horizontalAxisValue) > 0.01f)
             {
-                _groundSpawner.SpawnGround();
+                _currentMaterial = _sidePlayerMaterial;
+                rotation.Value = Quaternion.Euler(0, horizontalAxisValue < 0 ? 180 : 0, 0);
+            } 
+            else if (verticalAxisValue < 0f)
+            {
+                _currentMaterial = _frontPlayerMaterial;
             }
+
+            Graphics.DrawMesh(_quad, translation.Value, rotation.Value, _currentMaterial, 0);
+        
+            velocity.Linear += new float3(horizontalAxisValue * moveComponent.Speed,
+                verticalAxisValue > 0 ? verticalAxisValue * moveComponent.HorizontalForce : 0, 0) * Time.DeltaTime;
         });
-    }
-
-    private void MovePlayer(ref Entity entity, ref PhysicsVelocity velocity, ref MoveComponent moveComponent, ref Rotation rotation, ref Translation translation)
-    {
-        var horizontalAxisValue = Input.GetAxis("Horizontal");
-        var verticalAxisValue = Input.GetAxis("Vertical");
-        
-        if (Math.Abs(horizontalAxisValue) > 0.01f)
-        {
-            _currentMaterial = _sidePlayerMaterial;
-            rotation.Value = Quaternion.Euler(0, horizontalAxisValue < 0 ? 180 : 0, 0);
-        } 
-        else if (verticalAxisValue < 0f)
-        {
-            _currentMaterial = _frontPlayerMaterial;
-        }
-
-        Graphics.DrawMesh(_quad, translation.Value, rotation.Value, _currentMaterial, 0);
-        
-        velocity.Linear += new float3(horizontalAxisValue * moveComponent.Speed,
-            verticalAxisValue > 0 ? verticalAxisValue * moveComponent.HorizontalForce : 0, 0) * Time.DeltaTime;
     }
 }
