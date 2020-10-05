903bf0730100dc71399fb47eaed99562b0adec8d
Artem Yurchenko <artem@studiovector.pl>
2020-10-05 20:07:39 +0200

    UniRx was added; main menu gamepad controls were implemented

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+using UniRx;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    private IDisposable _startButtonStream;
+    private IDisposable _quitButtonStream;
+    
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+        
+        DisposeButtonsStreams();
+    }
+
+    private void DisposeButtonsStreams()
+    {
+        _startButtonStream.Dispose();
+        _quitButtonStream.Dispose();
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void Awake()
+    {
+        _startButtonStream = Observable.EveryUpdate()
+            .Where(_ => Input.GetButton("A Button"))
+            .Subscribe(_ => StartGame());
+
+        _quitButtonStream = Observable.EveryUpdate()
+            .Where(_ => Input.GetButton("B Button"))
+            .Subscribe(_ => Quit());
     }
 }
